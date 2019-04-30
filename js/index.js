ffa6f2202369773d5e4c440fdedab012020b07fd
iservan <iservan@gmail.com>
2019-04-30 19:16:04 +0200

    Adding more text content and design changes

--- js/index.js
+++ js/index.js
@@ -8,3 +8,17 @@ ScrollOut({
 });
 
 Splitting({ target: '.heading' });
+
+var mouse;
+
+
+/*function mouse_timeout() {
+  mouse = setTimeout(
+     document.getElementById('scroll-downs').style.opacity="1", 2000);
+}
+
+function myStopFunction() {
+  clearTimeout(mouse);
+}
+mouse_timeout();
+*/
