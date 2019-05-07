2d83358bc3a93721e3f9a4d72dc9c26608318b96
iservan <iservan@gmail.com>
2019-05-07 18:43:13 +0200

    Adding scrollreveal js library and effects

--- js/index.js
+++ js/index.js
@@ -9,16 +9,22 @@ ScrollOut({
 
 Splitting({ target: '.heading' });
 
-var mouse;
 
 
-/*function mouse_timeout() {
-  mouse = setTimeout(
-     document.getElementById('scroll-downs').style.opacity="1", 2000);
-}
 
-function myStopFunction() {
-  clearTimeout(mouse);
-}
-mouse_timeout();
-*/
+ScrollReveal().reveal('.logo', { 
+  delay: 700,
+  duration: 900,
+  easing: 'ease',
+  scale: 3,
+  mobile: false
+});
+
+var slideUp = {
+  delay: 1900,
+  duration: 500,
+  distance: '150%',
+  origin: 'bottom'
+};
+
+ScrollReveal().reveal('.scroll-down', slideUp);
